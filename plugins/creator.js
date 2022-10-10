f507b0e49953db96af44a40a259b6fda4043d088
ShiroNoMercy <108806813+Xavieroffcial@users.noreply.github.com>
2022-10-11 05:01:47 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,7 +6,7 @@ let name = await conn.getName(who)
 
 
 if (command == 'owner') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Maxxy;Bot;;Md\nFN:Saya Owner Maxxy Bot WhatsApp, Md\nNICKNAME:👑 Owner Maxxy Bot\nORG: Kahfiofc\nTITLE:soft\nitem1.TEL;waid=6285380166282:+62 853-8016-6282\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://lynk.id/kahfi.xd\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:maxxybotz@mail.com\nitem3.X-ABLabel:💌 Mail Owner MaxxyBot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 13 January 2001\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Filo;Bot;;Md\nFN:Saya Owner Filo Bot WhatsApp, Md\nNICKNAME:👑 Owner Filo Bot\nORG: Shiro\nTITLE:soft\nitem1.TEL;waid=6283862006229:+62 838-6200-6229\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://lynk.id/kahfi.xd\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:filobotz@mail.com\nitem3.X-ABLabel:💌 Mail Owner FiloBot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 1 September 2008\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
@@ -18,7 +18,7 @@ await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard
 if (command == 'creator') {
   try {
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `wudysoft@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://Maxxy-Md.github.io/`, `👤 Gada pawang nih senggol dong 😔`],
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `Filo@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://Maxxy-Md.github.io/`, `👤 Gada pawang nih senggol dong 😔`],
     [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://lynk.id/kahfi.xd/`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
   ], fakes)
   await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya ka😉`, sentMsg, {mentions: [m.sender]})
